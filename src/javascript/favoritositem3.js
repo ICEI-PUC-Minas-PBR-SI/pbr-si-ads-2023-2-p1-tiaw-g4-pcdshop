08e2c94ca9fda5cc5a51655cee93300a95efab79
Miguel Araújo <92738573+Miguelaraujo10@users.noreply.github.com>
2023-12-10 22:28:12 -0300

    Favoritos add

---
+++ src/javascript/favoritositem3.js
@@ -0,0 +1,23 @@
+function adicionarAosFavoritos(imagePatch) {
+  var itemName = "Bengala dobrável para deficientes visuais";
+  var itemPrice = 37.99;
+  var itemImage = imagePatch;
+  var favoritos = JSON.parse(localStorage.getItem("favoritos")) || [];
+  var itemExistente = favoritos.find(
+    (item) => item.name === itemName && item.image === imagePatch
+  );
+
+  if (!itemExistente) {
+    favoritos.push({
+      name: itemName,
+      price: itemPrice,
+      quantity: 1,
+      image: itemImage,
+    });
+    localStorage.setItem("favoritos", JSON.stringify(favoritos));
+    alert("Item adicionado aos favoritos!");
+    window.location.href = "favoritos.html"; // Redireciona para a página de favoritos
+  } else {
+    alert("Item já está na lista de favoritos!");
+  }
+}
