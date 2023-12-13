8e1d4197b3fc035ebccf84d6d107ac12befe3f20
luisfelipe2004 <luisfelipemarquesbernardes.com>
2023-12-13 03:54:19 -0300

    Alteração da tela de cadastro e login. Alteração da navbar das telas do projeto

--- src/cadastro.js
+++ src/cadastro.js
@@ -1,52 +1,91 @@
+    function storeUser(nome, data, email, telefone, senha) {
+    // Obter os usuários do localStorage
+    var usuariosCadastrados = JSON.parse(localStorage.getItem('usuarios')) || [];
 
-function cadastrarUsuario() {
-    // Obter os valores do formulário
-    var nome = document.getElementById('nome').value;
-    var data = document.getElementById('data-nascimento').value;
-    var email = document.getElementById('email').value;
-    var telefone = document.getElementById('telefone').value;
-    var senha = document.getElementById('senha').value;
-    var confirmarSenha = document.getElementById('confirmar-senha').value;
+    // Verificar se usuariosCadastrados é um array
+    if (!Array.isArray(usuariosCadastrados)) {
+        usuariosCadastrados = [];
+    }
 
-    // Verificar se as senhas coincidem
-    if (senha !== confirmarSenha) {
-        alert('As senhas não coincidem. Por favor, tente novamente.');
+    // Verificar se o email já está cadastrado
+    var emailJaCadastrado = false;
+    for (var i = 0; i < usuariosCadastrados.length; i++) {
+        if (usuariosCadastrados[i].email === email) {
+            emailJaCadastrado = true;
+            break;
+        }
+    }
+
+    if (emailJaCadastrado) {
+        alert('Email já cadastrado');
         return;
     }
 
-    // Verificar se o localStorage já contém dados
-    var usuarios = localStorage.getItem('usuarios');
-    if (!usuarios) {
-        // Se não houver dados, inicializar com um array vazio
-        usuarios = { "usuarios": [] };
-    } else {
-        // Se houver dados, converter a string JSON para um objeto
-        usuarios = JSON.parse(usuarios);
+        // Criar um objeto com os dados do usuário
+        var novoUsuario = {
+            nome: nome,
+            dataNascimento: data,
+            email: email,
+            telefone: telefone,
+            senha: senha
+        };
+
+        // Adicionar o novo usuário à lista de usuários cadastrados
+        usuariosCadastrados.push(novoUsuario);
+
+        // Armazenar a lista atualizada no localStorage
+        localStorage.setItem('usuarios', JSON.stringify(usuariosCadastrados));
     }
 
-    // Criar um novo usuário
-    var novoUsuario = {
-        "id": usuarios.usuarios.length + 1,
-        "nome": nome,
-        "data": data,
-        "email": email,
-        "telefone": telefone,
-        "senha": senha
-    };
+    // Adicionar um listener para o botão de envio
+    document.getElementById('envia').addEventListener('click', function(event) {
+        // Evitar que o formulário seja enviado, pois estamos manipulando isso via JavaScript
+        event.preventDefault();
 
-    // Adicionar o novo usuário ao array
-    usuarios.usuarios.push(novoUsuario);
+        // Obter os valores do formulário
+        var nome = document.getElementById('nome').value;
+        var data = document.getElementById('data-nascimento').value;
+        var email = document.getElementById('email').value;
+        var telefone = document.getElementById('telefone').value;
+        var senha = document.getElementById('senha').value;
+        var confirmarSenha = document.getElementById('confirmar-senha').value;
 
-    // Atualizar o localStorage com os novos dados
-    localStorage.setItem('usuarios', JSON.stringify(usuarios));
+        // Verificar se as senhas coincidem
+        if (senha !== confirmarSenha) {
+            alert('As senhas não coincidem. Por favor, tente novamente.');
+            return;
+        }
 
+        // Calcular a idade a partir da data de nascimento
+        var dataNascimento = new Date(data);
+        var hoje = new Date();
+        var idade = hoje.getFullYear() - dataNascimento.getFullYear();
+
+        // Verificar se a idade é maior ou igual a 18 anos
+        if (idade < 18) {
+            alert('Não é possível cadastrar menores de 18 anos');
+            return;
+        }
+
+        // Chama a função storeUser para armazenar o usuário
+        storeUser(nome, data, email, telefone, senha);
+
+        // Limpar o formulário
+        document.getElementById('form').reset();
+
+        // Redirecionar ou realizar outras ações após o cadastro bem-sucedido
+        alert('Usuário cadastrado com sucesso!');
+        window.location.href = "login.html";
+    });
+
+
+
+function cancelar()
+{
     // Limpar o formulário
     document.getElementById('form').reset();
-
-    alert('Usuário cadastrado com sucesso!');
-    window.location.href = "index.html";
-
+    window.location.href = "login.html";
 }
 
-// Adicionar um listener para o botão de envio
-document.getElementById('envia').addEventListener('click', cadastrarUsuario);
+
+
---
+++ src/login.js
@@ -0,0 +1,34 @@
+function storeUser() {
+  // Implemente a lógica de armazenamento do usuário aqui, se necessário
+  // Esta função pode ser implementada no arquivo de cadastro.js
+}
+
+function checkUser() {
+  var email = document.getElementById('email').value;
+  var password = document.getElementById('password').value;
+
+  // Obter os usuários do localStorage
+  var usuariosCadastrados = JSON.parse(localStorage.getItem('usuarios')) || [];
+
+  // Verificar se o usuário existe
+  var usuarioExistente = usuariosCadastrados.find(function(usuario) {
+      return usuario.email === email && usuario.senha === password;
+  });
+
+  if (usuarioExistente) {
+      alert('Login bem-sucedido!');
+      // Redirecionar ou realizar outras ações após o login bem-sucedido
+      window.location.href = "index.html";
+  } else {
+      alert('Email ou senha incorretos. Por favor, tente novamente.');
+  }
+}
+
+// Adicionar um listener para o botão de login
+document.querySelector('.btnSubmit').addEventListener('click', function(event) {
+  event.preventDefault(); // Impede o envio do formulário
+
+  // Chama as funções storeUser e checkUser
+  storeUser();
+  checkUser();
+});
